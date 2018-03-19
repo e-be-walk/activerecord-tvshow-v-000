76379b266989e90908ba6bab05d61baba3129ce5
Erin Walker <erinb0317@hotmail.com>
2018-03-19 00:47:15 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -22,5 +22,13 @@ class Show < ActiveRecord::Base
     Show.sum(:rating)
   end
 
+  def self.popular_shows 
+    Show.where("rating > 5")
+  end 
+
+  def self.shows_by_alphabetical_order 
+    Show.order(name: :asc)
+  end 
+
 
 end
