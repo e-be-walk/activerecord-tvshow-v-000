f87a5edd7181df91cc8a23fce6dcc47ca828053d
Erin Walker <erinb0317@hotmail.com>
2018-03-19 00:41:00 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,24 @@
 class Show < ActiveRecord::Base
 
   def highest_rating
+    Show.maximum("rating")
   end
 
   def most_popular_show
+    Show.maximum("rating")
+  end
+
+  def lowest_rating 
+    Show.minimum("rating")
   end 
+
+  def least_popular_show
+    Show.minimum("rating")
+  end 
+
+  def ratings_sum
+    Show.sum("rating")
+  end 
+
+  
 end
